de41629ff4170999c5cd24cdec911625cb9cc38c
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-04-30 21:55:26 +0430

    add style components

--- app/component/Product.jsx
+++ app/component/Product.jsx
@@ -2,9 +2,7 @@ import Image from 'next/image';
 const Product = ({ productSingel }) => {
   return (
     <div className="container m-auto  flex gap-6">
-      <div className="ml-10">
         <Image width={500} height={50} src={productSingel.image} alt="image" />
-      </div>
       <div className="flex flex-col justify-end m-10">
         <div className="text-2xl">
           <p> کدکالا :{productSingel.id}</p>
@@ -13,12 +11,13 @@ const Product = ({ productSingel }) => {
           <p> توضیحات :{productSingel.description}</p>
         </div>
         <button
-          className="p-4 bg-blue-500 mt-4 text-xl font-bold hover:bg-blue-600
+          className="p-4 bg-blue-400 mt-4 text-xl font-bold hover:bg-blue-600
            rounded-md hover:text-white duration-700"
         >
           اضافه کردن به سبد خرید
         </button>
       </div>
+      <Image width={500} height={50} src={productSingel.imageb} alt="image" />
     </div>
   );
 };
