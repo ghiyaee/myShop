a33d7536c56e17efd17e07bd055557265304ea01
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-05-02 17:16:15 +0430

    update cart quantity list items

--- app/component/Card.jsx
+++ app/component/Card.jsx
@@ -11,6 +11,10 @@ const Card = () => {
   const removeProduct = (i) => {
     dispatch({type: 'REMOVE_PRODUCT',payload:i})
   }
+  const updateGty = (i, gty) => {
+    const quantity=+ gty
+    dispatch({ type: 'CART_ADD_ITEM' ,payload:{...i,quantity}});
+  }
   return (
     <>
       <div className="flex gap-10">
@@ -38,7 +42,18 @@ const Card = () => {
                 <h2 className="text-xl font-bold flex justify-center items-center w-16 ">
                   {resp.name}
                 </h2>
-                <p>{resp.quantity}</p>
+                <p>
+                  <select
+                    value={resp.quantity}
+                    onChange={(e) => updateGty(resp, e.target.value)}
+                  >
+                    {[...Array(resp.countInStack).keys()].map((c) => (
+                      <option key={c + 1} value={c + 1}>
+                        {c + 1}
+                      </option>
+                    ))}
+                  </select>
+                </p>
                 <p className="">قیمت :{resp.price} تومان</p>
                 <button
                   className="primery-button "
@@ -52,13 +67,21 @@ const Card = () => {
         </div>
         <div className="shadow-xl w-[50%] ml-6 text-center p-6 text-3xl flex flex-col justify-around">
           <div>
-            {`جمع اقلام (${cart.cartItems.reduce((a, b) => a + b.quantity, 0)}) 
+            {`جمع تعداد واقلام (${cart.cartItems.reduce(
+              (a, b) => a + b.quantity,
+              0
+            )}) 
           ${cart.cartItems.reduce(
             (a, b) => a + b.quantity * b.price,
             0
           )} تومان`}
           </div>
-          <button className="primery-button w-full" onClick={()=> router.push('/checkOut')} >سفارش نهایی</button>
+          <button
+            className="primery-button w-full"
+            onClick={() => router.push('/checkOut')}
+          >
+            سفارش نهایی
+          </button>
         </div>
       </div>
     </>
